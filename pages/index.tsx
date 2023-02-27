4d3bc2174f3de7e899d1fbe72bc2a600fc1a51a9
hailey <gmlthd0908@gmail.com>
2023-02-27 16:38:37 +0900

    feat: svg setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import { Button } from "antd";
-import { Checkbox } from "@/components/checkbox";
 import React from "react";
+import { Inter } from "@next/font/google";
+import { Button } from "antd";
+import { Checkbox } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
